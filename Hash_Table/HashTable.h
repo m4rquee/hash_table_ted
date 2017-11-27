71d0c9404090ef0c3616c553127406b737cc06db
LucasXM12 <lucasxm21@hotmail.com>
2017-11-27 09:01:01 -0200

    Constructure

--- Hash_Table/HashTable.h
+++ Hash_Table/HashTable.h
@@ -16,14 +16,18 @@ public:
 	~HashTable();
 	HashTable(unsigned int);
 
+	void addValue(KeyType, DataType);
+
 private:
 	int size;
 
 	struct Item {
 		KeyType* key;
 		DataType* data;
+
 		Item* next;
-	} Item;
+		unsigned int length;
+	};
 
 	Item* table;
 };
@@ -35,15 +39,20 @@ HashTable<KeyType, DataType>::HashTable(unsigned int size) {
 
 	this->size = size;
 	this->table = new Item[this->size];
+
+	for (unsigned int i = 0; i < size; i++)
+		this->table[i] = nullptr;
 }
 
 template<class KeyType, typename DataType>
-HashTable<KeyType, DataType>::HashTable()
-{
-}
+HashTable<KeyType, DataType>::HashTable() {}
 
 template<class KeyType, typename DataType>
-HashTable<KeyType, DataType>::~HashTable() {
+HashTable<KeyType, DataType>::~HashTable() {}
+
+template<class KeyType, typename DataType>
+void HashTable<KeyType, DataType>::addValue(KeyType key, DataType data) {
+	unsigned int pos = key.hashCode();
 }
 
 #endif // HASH_TABLE
